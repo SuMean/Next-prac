dc6a5c7f18f2af77a2867e872908e14c61bd8116
sam101321 <sam010321@gmail.com>
2023-07-02 20:40:53 +0900

    test

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -6,18 +6,11 @@ export default function Cart() {
       <CartItem item={장바구니[0]} />
       <CartItem item={장바구니[1]} />
       <CartItem item={장바구니[2]} />
-      <Btn color="blue"/>
     </div>
   );
 }
 interface CartItemProps {
   item: string;
-}
-interface BtnProps {
-  color: string;
-}
-function Btn(props: BtnProps) {
-  return <button style={{ background: props.color }}> 버튼 </button>;
 }
 
 function CartItem(props: CartItemProps) {
