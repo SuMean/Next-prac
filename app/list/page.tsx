2a2e0c88f121dbf5cf12cdd2776b50f3fa11ddcb
sam101321 <sam010321@gmail.com>
2023-07-02 21:43:43 +0900

    list button

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -4,20 +4,33 @@ import { useState } from "react";
 
 export default function List() {
   const 상품 = ["Tomatoes", "Pasta", "Coconut"];
-  const [수량, 수량변경] = useState(0);
+  const [수량, 수량변경] = useState([0, 0, 0]);
   return (
     <div>
       <h4 className="title">상품목록</h4>
+
+    
 
       {상품.map((a, i) => {
         return (
           <div className="food" key={i}>
             <img src={`/food${i}.png`} className="food-img" />
             <h4>{상품[i]} $40</h4>
-            <span>{수량}</span>
             <button
               onClick={() => {
-                수량변경(수량 + 1);
+                const copy = [...수량];
+                copy[i]--;
+                수량변경(copy);
+              }}
+            >
+              -
+            </button>
+            <span>{수량[i]}</span>
+            <button
+              onClick={() => {
+                const copy = [...수량];
+                copy[i]++;
+                수량변경(copy);
               }}
             >
               +
